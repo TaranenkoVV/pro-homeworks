16eda29119e2aec217b4eeb2946b36fc704ea702
vvltaranenko <tvvchaik@mail.ru>
2024-06-08 11:33:10 +0300

    first commit

---
+++ homeworks/src/test/java/ru/taranenko/homeworks/homework24/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.taranenko.homeworks.homework24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
